fc2878a68d5e5b9f412717c73248248aa20b0a31
eduardasch <108293617+eduardasch@users.noreply.github.com>
2022-07-11 16:11:38 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function menushow(){
-    let menumobile = document.querySelector(''.menumobile');
+    let menumobile = document.querySelector('.menumobile');
     if (menumobile.classlist.contains('open')){
         menumobile.classlist.remuve('open');
-    } else {
-        menumobile.classlist.add('open')
+    } 
+    else {
+        menumobile.classlist.add('open');
     }
 }
